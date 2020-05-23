a1eb22cbe50a41748f011058e8c90f376a796b0d
rohan <rohanrajput16.phy@gmail.com>
2020-05-23 14:13:55 +0530

    initial commmit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+let buttn = document.querySelector('.btn').addEventListener("click", function(e){
+    alert('Login Sucessfully')
+});
+
+
+function changeLogin(id)
+{
+    id.innerHTML="Logged In"
+}
+
+
